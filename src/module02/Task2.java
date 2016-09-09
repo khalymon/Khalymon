a2d680c7b8d12a0b57265c2d14e1fd04d8ca92e5
Oleg Orlov <info@olegorlov.com>
2016-09-09 09:29:18 +0300

    Замечания от ментора к модуль2 таск2

--- src/module02/Task2.java
+++ src/module02/Task2.java
@@ -1,10 +1,27 @@
 package module02;
 
 public class Task2 {
+
+    /*
+    Обычно модификатор доступа у финальной статической переменной private
+     */
     public static final double commission = 0.05;
 
+    /*
+    Хорошо, правда есть куда улучшить
+    если ты напишешь в этом методе
+    return balance - withdrawal - withdrawal * commission;
+    и вынесешь в отдельный метод withdrawal * commission
+    будет лучше.
+     */
     static double withdrawBalance(double balance, double withdrawal) {
-        return balance - (commission + 1) * withdrawal;
+//        return balance - (commission + 1) * withdrawal;
+//        return balance - withdrawal - withdrawal * commission;
+        return balance - withdrawal - getCommission(withdrawal);
+    }
+
+    private static double getCommission(double withdrawal) {
+        return withdrawal * commission;
     }
 
     public static void main(String[] args) {
@@ -14,7 +31,8 @@ public class Task2 {
         if (balanceAfterWithdrawal < 0) {
             System.out.println("NO");
         } else {
-            System.out.println("OK " + commission * withdrawal + " " + balanceAfterWithdrawal);
+//            System.out.println("OK " + commission * withdrawal + " " + balanceAfterWithdrawal);
+            System.out.println("OK " + getCommission(withdrawal) + " " + balanceAfterWithdrawal);
         }
 
         balance = 100;
@@ -23,7 +41,8 @@ public class Task2 {
         if (balanceAfterWithdrawal < 0) {
             System.out.println("NO");
         } else {
-            System.out.println("OK " + commission * withdrawal + " " + balanceAfterWithdrawal);
+//            System.out.println("OK " + commission * withdrawal + " " + balanceAfterWithdrawal);
+            System.out.println("OK " + getCommission(withdrawal) + " " + balanceAfterWithdrawal);
         }
     }
 }
