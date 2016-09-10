c5a738cf50536b20de66626eddd6a694409f750f
garfield <holyman.login@gmail.com>
2016-09-10 17:58:20 +0300

    initial commit

---
+++ src/module04/task6/Main.java
@@ -0,0 +1,22 @@
+package module04.task6;
+
+import module04.task4.User;
+
+public class Main {
+    User[] users = new User[6];
+
+
+
+
+//    User user01;
+//    User user02;
+//    User user03;
+//    User user04;
+//    User user05;
+//    User user06;
+
+
+
+
+
+}
