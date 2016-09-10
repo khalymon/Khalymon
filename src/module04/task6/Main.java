4e92d0182d0858bc51f9d22888e7c64bedbd41e5
garfield <holyman.login@gmail.com>
2016-09-10 18:49:51 +0300

    Initial commit

--- src/module04/task6/Main.java
+++ src/module04/task6/Main.java
@@ -6,6 +6,7 @@ import module04.task1.Bank;
 import module04.task3.USBank;
 import module04.task3.EUBank;
 import module04.task3.ChinaBank;
+import module04.task5.BankSystemImpl;
 
 
 public class Main {
@@ -18,6 +19,7 @@ public class Main {
             USBank usBank       =    new USBank(100001, "USA", Currency.USD, 20000, 1000, 1, (10_000_000_000_000L));
             EUBank euBank       =    new EUBank(100002, "EUR", Currency.EUR,  5000,  800, 2,  (2_000_000_000_000L));
             ChinaBank chinaBank = new ChinaBank(100055, "CNY", Currency.USD, 40000,  100, 3, (50_000_000_000_000L));
+            BankSystemImpl bankSystemImpl = new BankSystemImpl();
 
             users[0].setBank(usBank);
             users[1].setBank(usBank);
@@ -25,6 +27,28 @@ public class Main {
             users[3].setBank(euBank);
             users[4].setBank(chinaBank);
             users[5].setBank(chinaBank);
+
+            users[0].setSalary(100);
+            users[1].setSalary(50);
+            users[2].setSalary(50);
+            users[3].setSalary(50);
+            users[4].setSalary(200);
+            users[5].setSalary(300);
+
+            users[0].setBalance(1000);
+            users[1].setBalance(500);
+            users[2].setBalance(500);
+            users[3].setBalance(500);
+            users[4].setBalance(2000);
+            users[5].setBalance(3000);
+
+            for(int i = 0; i<users.length; i++){
+                bankSystemImpl.paySalary(users[i]);
+                bankSystemImpl.fundUser(users[i],10);
+                bankSystemImpl.withdrawOfUser(users[i],1);
+                bankSystemImpl.transferMoney(users[i], users[(i+1)%6], 5);
+            }
+
         }
 
 
