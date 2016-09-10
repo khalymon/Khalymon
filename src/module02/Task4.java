ad83105265315915e7a74b21f63f02d3dfb5817c
Oleg Orlov <info@olegorlov.com>
2016-09-10 22:37:28 +0300

    new version module2 task4

--- src/module02/Task4.java
+++ src/module02/Task4.java
@@ -2,26 +2,29 @@ package module02;
 
 public class Task4 {
 
-/*
-    Посмотри замечания (минусы) к третьему заданию, они почти все сюда применимы
-*/
+    static int[] balances = {1200, 250, 2000, 500, 3200};
+    static String[] ownerNames = {"Jane", "Ann", "Jack", "Oww", "Lane"};
+
     static double fundBalance(String ownerName, double fund) {
-        int[] balances = {1200, 250, 2000, 500, 3200};
-        String[] ownerNames = {"Jane", "Ann", "Jack", "Oww", "Lane"};
-        int ownerID = -1;
-        for (int i = 0; i < ownerNames.length; i++) {
-            if (ownerNames[i].equals(ownerName)) {
-                ownerID = i;
-                break;
-            }
-        }
-        if (ownerID == -1) {
+        int ownerIndex = getOwnerIndex(ownerName);
+        if (ownerIndex < 0) {
             return -1;
         }
-        return balances[ownerID] + fund;
+        balances[ownerIndex] += fund;
+        return balances[ownerIndex];
+    }
+
+    private static int getOwnerIndex(String ownerName) {
+        for (int i = 0; i < ownerNames.length; i++) {
+            if (ownerNames[i].equals(ownerName)) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     public static void main(String[] args) {
+
         String ownerName = "Oww";   //існує в масиві
         double fund = 100;
         double balanceAfterFunding = fundBalance(ownerName, fund);
