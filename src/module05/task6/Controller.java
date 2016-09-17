103d0e9cc65f9cb963b99c6905b1efe768861723
garfield <holyman.login@gmail.com>
2016-09-17 16:01:33 +0300

    module05 ini

--- src/module05/task6/Controller.java
+++ src/module05/task6/Controller.java
@@ -1,5 +1,6 @@
 package module05.task6;
 
+import module05.task1_2.Room;
 import module05.task3.API;
 
 import static module05.task7.Main.apisGlobal;
@@ -7,5 +8,12 @@ import static module05.task7.Main.apisGlobal;
 public class Controller {
     private API[] apis = apisGlobal;
 
+    public Room[] requestedRooms(int price, int persons, String city, String hotel){
+        return null;
+    }
+
+    public Room[] check(API api1, API api2){
+        return null;
+    }
 
 }
