743e1341603cd77a7353dc9487332d89116ef1c8
garfield <holyman.login@gmail.com>
2016-09-24 20:55:22 +0300

    module06 ini

--- src/module06/UserUtils.java
+++ src/module06/UserUtils.java
@@ -4,40 +4,73 @@ public class UserUtils {
 
     private User[] uniqueUsersForPos(User[] users, int pos) {
         // correct position values: pos = 0..(users.length-2)
-        User[] immutableUsers = new User[pos];
+        User[] immutableUsers;// = new User[pos];
         User[] mutableUsers;
         User[] toReturn;
 
-        for (int i = 0; i < pos; i++) {
-            immutableUsers[i] = users[i];
-        }
+        if (pos == 0) {
 
-        int mutableLengthCounter = 0;
-        for (int i = pos + 1; i < users.length; i++) {
-            if (users[pos].getId() == users[i].getId()) {
-                mutableLengthCounter++;
+            int mutableLengthCounter = 0;
+            for (int i = pos + 1; i < users.length; i++) {
+                if (users[pos].getId() == users[i].getId()) {
+                    mutableLengthCounter++;
+                }
             }
-        }
 
-        mutableUsers = new User[users.length - pos - mutableLengthCounter];
-        mutableUsers[0] = users[pos];
-        int j = 0; // допоміжна змінна циклу
-        for (int i = pos + 1; i < users.length; i++) {
-            if (users[pos].getId() != users[i].getId()) {
-                mutableUsers[j] = users[i];
-                j++;
+            mutableUsers = new User[users.length - pos - mutableLengthCounter];
+            mutableUsers[0] = users[pos];
+            int j = 0; // допоміжна змінна циклу
+            for (int i = pos + 1; i < users.length; i++) {
+                if (users[pos].getId() != users[i].getId()) {
+                    mutableUsers[j] = users[i];
+                    j++;
+                }
             }
+
+            toReturn = new User[mutableUsers.length];
+            for (int i = 0; i < mutableUsers.length; i++) {
+                toReturn[i] = mutableUsers[i];
+            }
+
+            return toReturn;
+
+
+
+        } else {
+            immutableUsers = new User[pos];
+
+            for (int i = 0; i < pos; i++) {
+                immutableUsers[i] = users[i];
+            }
+
+            int mutableLengthCounter = 0;
+            for (int i = pos + 1; i < users.length; i++) {
+                if (users[pos].getId() == users[i].getId()) {
+                    mutableLengthCounter++;
+                }
+            }
+
+            mutableUsers = new User[users.length - pos - mutableLengthCounter];
+            mutableUsers[0] = users[pos];
+            int j = 0; // допоміжна змінна циклу
+            for (int i = pos + 1; i < users.length; i++) {
+                if (users[pos].getId() != users[i].getId()) {
+                    mutableUsers[j] = users[i];
+                    j++;
+                }
+            }
+
+            toReturn = new User[immutableUsers.length + mutableUsers.length];
+            for (int i = 0; i < immutableUsers.length; i++) {
+                toReturn[i] = immutableUsers[i];
+            }
+            for (int i = 0; i < mutableUsers.length; i++) {
+                toReturn[i + immutableUsers.length] = mutableUsers[i];
+            }
+
+            return toReturn;
         }
 
-        toReturn = new User[immutableUsers.length + mutableUsers.length];
-        for (int i = 0; i < immutableUsers.length; i++) {
-            toReturn[i] = immutableUsers[i];
-        }
-        for (int i = 0; i < mutableUsers.length; i++) {
-            toReturn[i + immutableUsers.length] = mutableUsers[i];
-        }
-
-        return toReturn;
     }
 
     public User[] uniqueUsers(User[] users) {
@@ -47,11 +80,7 @@ public class UserUtils {
             return usersToReturn;
         } else {
             int posCounter = 0;
-//            do {
-//                usersToReturn = uniqueUsersForPos(usersToReturn, posCounter);
-//                posCounter--;
-//            }
-//            while (posCounter <= usersToReturn.length - 2);
+
             while (posCounter <= usersToReturn.length - 2) {
                 usersToReturn = uniqueUsersForPos(usersToReturn, posCounter);
                 posCounter++;
