d9e2d5a825fd738c22b1b63f5791203f9e2037e0
garfield <holyman.login@gmail.com>
2016-10-15 16:21:54 +0300

    ini

--- src/module05/BookingComAPI.java
+++ src/module05/BookingComAPI.java
@@ -7,34 +7,17 @@ import java.util.List;
 
 public class BookingComAPI implements API {
 
-    //    private Room[] rooms;
     private List<Room> rooms;
 
     public BookingComAPI() {
         Calendar calendar = Calendar.getInstance();
         calendar.set(2016, Month.SEPTEMBER.getValue(), 1);
-//        rooms = new Room[]{
-//                new Room(1100L, 1500, 2, calendar.getTime(), "Royal", "Paris"),
-//                new Room(1101L, 1000, 1, calendar.getTime(), "Royal", "London"),
-//                new Room(1102L, 2500, 2, calendar.getTime(), "Intercontinental", "Paris"),
-//        };
-        rooms = new ArrayList<Room>(
-                new Room(1100L, 1500, 2, calendar.getTime(), "Royal", "Paris"),
-                new Room(1101L, 1000, 1, calendar.getTime(), "Royal", "London"),
-                new Room(1102L, 2500, 2, calendar.getTime(), "Intercontinental", "Paris"),
-                );
+        rooms = new ArrayList<Room>();
+        rooms.add(new Room(1100L, 1500, 2, calendar.getTime(), "Royal", "Paris"));
+        rooms.add(new Room(1101L, 1000, 1, calendar.getTime(), "Royal", "London"));
+        rooms.add(new Room(1102L, 2500, 2, calendar.getTime(), "Intercontinental", "Paris"));
     }
 
-    //    public Room[] findRooms(int price, int persons, String hotel, String city) {
-//        List<Room> roomsList = new ArrayList<Room>();
-//        Room lookingRoom = new Room(0L, price, persons, Calendar.getInstance().getTime(), hotel, city);
-//        for (Room room : rooms) {
-//            if (room.equals(lookingRoom)) {
-//                roomsList.add(room);
-//            }
-//        }
-//        return roomsList.toArray(new Room[roomsList.size()]);
-//    }
     public List<Room> findRooms(int price, int persons, String hotel, String city) {
         List<Room> roomsList = new ArrayList<Room>();
         Room lookingRoom = new Room(0L, price, persons, Calendar.getInstance().getTime(), hotel, city);
@@ -43,14 +26,10 @@ public class BookingComAPI implements API {
                 roomsList.add(room);
             }
         }
-        //   return roomsList.toArray(new Room[roomsList.size()]);
         return roomsList;
     }
 
     @Override
-//    public Room[] getAll() {
-//        return rooms;
-//    }
     public List<Room> getAll() {
         return rooms;
     }
