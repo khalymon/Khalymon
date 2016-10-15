b579b30d0eddf83c846fcd8e18246358ed6bbdd8
garfield <holyman.login@gmail.com>
2016-10-15 17:29:47 +0300

    ini

--- src/module07/task4/Controller.java
+++ src/module07/task4/Controller.java
@@ -1,7 +1,6 @@
 package module07.task4;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class Controller {
@@ -23,19 +22,10 @@ public class Controller {
         return result;
     }
 
-    public Room[] check(API api1, API api2, int price, int persons, String hotel, String city) {
-        Room[] result = new Room[0];
-        Room[] roomsApi1 = api1.getAll();
-        Room[] roomsApi2 = api2.getAll();
-        for (int i = 0; i < roomsApi1.length; i++) {
-            for (int j = 0; j < roomsApi2.length; j++) {
-                if (roomsApi1[i].equals(roomsApi2[j])) {
-                    result = Arrays.copyOf(result, result.length + 1);
-                    result[result.length - 1] = roomsApi1[i];
-                    break;
-                }
-            }
-        }
+    public List<Room> check(API api1, API api2, int price, int persons, String hotel, String city) {
+        List<Room> result = api1.getAll();
+        List<Room> roomsApi2 = api2.getAll();
+        result.retainAll(roomsApi2);
         return result;
     }
 
