8fbd605166fbe598977482b09fdc29babeea59af
garfield <holyman.login@gmail.com>
2016-10-15 19:15:16 +0300

    ini

--- src/module08/Main.java
+++ src/module08/Main.java
@@ -1,7 +1,23 @@
 package module08;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Here.");
+//        List<Integer> integerList = new ArrayList<Integer>();
+//        integerList.add(1);
+//        integerList.add(1);
+//        integerList.add(2);
+//        integerList.add(3);
+//        integerList.add(5);
+//        integerList.add(8);
+//        for (int i = 0; i < integerList.size(); i++) {
+//            System.out.println(integerList.get(i) + " ");
+//        }
+//        System.out.println();
+//        System.out.println("Here.");
+//        System.out.println(integerList.getClass().getSimpleName());
+//        System.out.println(integerList.get(0).getClass().getSimpleName());
     }
 }
