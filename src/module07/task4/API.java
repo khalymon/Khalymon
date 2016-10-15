12796c50cf1dc56b90fd982f0ed4e1d52e45d3a1
garfield <holyman.login@gmail.com>
2016-10-15 17:24:07 +0300

    ini

--- src/module07/task4/API.java
+++ src/module07/task4/API.java
@@ -4,10 +4,8 @@ import java.util.List;
 
 public interface API {
 
-    //    Room[] findRooms(int price, int persons, String hotel, String city);
     List<Room> findRooms(int price, int persons, String hotel, String city);
 
-    //    Room[] getAll();
     List<Room> getAll();
 
 }
--- src/module07/task4/Controller.java
+++ src/module07/task4/Controller.java
@@ -2,7 +2,6 @@ package module07.task4;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 public class Controller {
@@ -16,25 +15,10 @@ public class Controller {
         };
     }
 
-//    public Room[] requestedRooms(int price, int persons, String hotel, String city) {
-//        Room[] result = new Room[0];
-//        for (int i = 0; i < apis.length; i++) {
-//            Room[] rooms = apis[i].findRooms(price, persons, hotel, city);
-//            int resultLength = result.length;
-//            int apiRoomsLength = rooms.length;
-//            result = Arrays.copyOf(rooms, resultLength + apiRoomsLength);
-//            for (int k = resultLength; k < result.length; k++) {
-//                result[k] = rooms[k - resultLength];
-//            }
-//        }
-//        return result;
-//    }
-
-    public List<Room> requestedRooms(int price, int persons, String hotel, String city){
+    public List<Room> requestedRooms(int price, int persons, String hotel, String city) {
         List<Room> result = new ArrayList<Room>();
-        for (int i = 0; i < apis.length; i++){
-            List<Room> rooms = new ArrayList<Room>();
-            Collections.addAll(rooms, apis[i].findRooms(price, persons, hotel, city))
+        for (API api : apis) {
+            result.addAll(api.findRooms(price, persons, hotel, city));
         }
         return result;
     }
