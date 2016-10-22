e63af904631cfd9e9615d080e45d41d3498064ad
garfield <holyman.login@gmail.com>
2016-10-22 18:35:44 +0300

    ini

--- src/module09/Main.java
+++ src/module09/Main.java
@@ -142,9 +142,9 @@ public class Main {
 
         System.out.println("[orderList]. Deleting duplicates...");
         printSeparator("-");
-
-        orderList = (List<Order>) orderList.stream().collect(Collectors.toMap(p -> p, p -> p)).values();
+        // orderList = (List<Order>) orderList.stream().collect(Collectors.toMap(p -> p, p -> p)).values();
         //orderList.stream().distinct();
+        orderList = orderList.stream().map(p -> p).collect(Collectors.toList());
         System.out.println(Order.ordersCollectionToString(orderList));
         printSeparator("=");
 
