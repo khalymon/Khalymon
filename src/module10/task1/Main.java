0f2212fe1549cd1a21f67304de61ad37e34a0267
garfield <holyman.login@gmail.com>
2016-10-23 18:13:47 +0300

    ini

--- src/module10/task1/Main.java
+++ src/module10/task1/Main.java
@@ -1,19 +1,13 @@
 package module10.task1;
 
-public class Main extends Exception{
-    public Main(String message) {
-        super(message);
-    }
-
+public class Main {
     public static void main(String[] args) throws Exception {
         try {
             System.out.println("Here.");
             throw new Exception("New exception in Main tread.");
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             System.out.println(e.getMessage());
-        }
-        finally {
+        } finally {
             System.out.println("finally");
         }
     }
