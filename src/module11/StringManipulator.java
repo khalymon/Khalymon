d268146f0546ec6d919c848806c37f8c6acd40cb
garfield <holyman.login@gmail.com>
2016-11-05 15:58:05 +0200

    ini

---
+++ src/module11/StringManipulator.java
@@ -0,0 +1,9 @@
+package module11;
+
+public class StringManipulator {
+    private static final String string = "THE TRUTH IS OUT THERE";
+
+    public String replacer(){
+        return null;
+    }
+}
