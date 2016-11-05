37fdb8cdbcb57d12e2c833482bdcf5f114d28ccb
garfield <holyman.login@gmail.com>
2016-11-05 16:05:54 +0200

    ini

--- src/module11/StringManipulator.java
+++ src/module11/StringManipulator.java
@@ -1,9 +1,30 @@
 package module11;
 
+import java.io.File;
+import java.util.Map;
+
 public class StringManipulator {
     private static final String string = "THE TRUTH IS OUT THERE";
 
-    public String replacer(){
+    private File file;
+
+    public StringManipulator(File file) {
+        this.file = file;
+    }
+
+    public String replacer(Map<String, String> map) {
         return null;
+    }
+
+    public File fileContentReplacer(Map<String, String> map) {
+        return file;
+    }
+
+    public File fileContentMerger(Map<String, String> map) {
+        return file;
+    }
+
+    int checkWord(String word) {
+        return 0;
     }
 }
